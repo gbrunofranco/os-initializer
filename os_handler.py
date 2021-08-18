211f3c526a5245ce93612aa36a2d2395c233eb22
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2021-08-18 10:54:14 +0200

    Initial commit

---
+++ os_handler.py
@@ -0,0 +1,129 @@
+from abc import ABC, abstractmethod
+from subprocess import call, run
+
+
+class Os(ABC):
+    """Abstract class inherited by every OS subclass"""
+
+    def __init__(self):
+        self.name: str = get_current_os_name()
+        self.install_command: str = self.get_os_install_command()
+        self.update_command: str = self.get_os_update_command()
+        self.prepare_commands: list[str] = self.get_os_prepare_commands()
+        self.cleanup_command: list[str] = self.get_os_cleanup_commands()
+        self.packages: list
+
+    def prepare(self):
+
+        self.run_command(self.update_command)
+
+        for command in self.prepare_commands:
+            self.run_command(command)
+
+        for package in self.packages:
+            self.install_command(package)
+
+        for command in self.cleanup_command:
+            self.run_command(command)
+
+    @staticmethod
+    def run_command(command):
+        try:
+            call(f"{command}", shell=True)
+        except OSError as e:
+            print(f"Execution of preparation {command} failed.", e)
+
+    @staticmethod
+    @abstractmethod
+    def get_os_install_command():
+        """Returns the package installation command for the current OS"""
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_os_update_command():
+        """Returns the system-wide package update command for the current OS"""
+        pass
+
+    @abstractmethod
+    def get_os_prepare_commands(self):
+        """Returns the commands that run before the installation of packages"""
+        pass
+
+    @abstractmethod
+    def get_os_cleanup_commands(self):
+        """Returns the commands that run after the installation of packages"""
+        pass
+
+
+class ArcoLinux(Os):
+    """Handler for ArcoLinux system"""
+
+    packages = [
+        'tint2',
+        'openbox',
+    ]
+
+    @staticmethod
+    def get_os_install_command() -> str:
+        return "pacman -S"
+
+    @staticmethod
+    def get_os_update_command() -> str:
+        return "pacman -Syu"
+
+    def get_os_prepare_commands(self) -> list[str]:
+        prepare_commands = [
+            "git clone git@github.com:gbrunofranco/dotfiles.git",
+            "sudo cp ./dotfiles/etc/X11/xorg.conf.d/00-keyboard.conf ./dotfiles/etc/X11/xorg.conf.d/"
+        ]
+        return prepare_commands
+
+    def get_os_cleanup_commands(self) -> list[str]:
+        cleanup_commands = [
+            "rm -rf ./dotfiles"
+        ]
+        return cleanup_commands
+
+
+class ArchLinux(Os):
+    """Handler for ArchLinux system"""
+
+    packages = [
+        'tint2',
+        'openbox',
+    ]
+
+    @staticmethod
+    def get_os_install_command() -> str:
+        return "pacman -S"
+
+    @staticmethod
+    def get_os_update_command() -> str:
+        return "pacman -Syu"
+
+    def get_os_prepare_commands(self) -> list[str]:
+        prepare_commands = [
+            "git clone git@github.com:gbrunofranco/dotfiles.git",
+            "sudo cp ./dotfiles/etc/X11/xorg.conf.d/00-keyboard.conf ./dotfiles/etc/X11/xorg.conf.d/"
+        ]
+        return prepare_commands
+
+    def get_os_cleanup_commands(self) -> list[str]:
+        cleanup_commands = [
+            "rm -rf ./dotfiles"
+        ]
+        return cleanup_commands
+
+
+def get_current_os_class():
+    return globals()[get_current_os_name()]
+
+
+def get_current_os_name() -> str:
+    return run(f"grep '^NAME=' /etc/os-release", shell=True, capture_output=True).stdout.decode("utf-8").strip()[5:].replace(' ', '')
+
+
+if __name__ == '__main__':
+    current_os = get_current_os_class()()
+    current_os.prepare()
---
+++ test_os.py
@@ -0,0 +1,91 @@
+"""
+OS class tests.
+"""
+
+import unittest
+from abc import ABCMeta
+
+from os_handler import ArcoLinux, ArchLinux, Os, get_current_os_class, get_current_os_name
+
+
+class TestHelperFunctions(unittest.TestCase):
+    "Test the helper function of the os_handler module"
+
+    def test_get_current_os_name_returns_string(self):
+        self.assertIsInstance(get_current_os_name(), str)
+
+    def test_get_current_os_class_returns_class(self):
+        supported_oses_class = [ArcoLinux]
+        self.assertIn(get_current_os_class(), supported_oses_class)
+
+
+class TestArcolinuxInitialization(unittest.TestCase):
+    "Test the initialize method of Arcolinux class."
+
+    def setUp(self):
+        """Setup test fixtures"""
+        self.working_machine_os = ArcoLinux()
+
+    def test_get_os_install_command_returns_string(self):
+        "Whether get_os_install_command returns a string"
+        self.assertIsInstance(
+            self.working_machine_os.get_os_install_command(), str)
+
+    def test_get_os_update_command_returns_string(self):
+        "Whether get_os_update_command returns a string"
+        self.assertIsInstance(
+            self.working_machine_os.get_os_update_command(), str)
+
+    def test_get_os_prepare_commands_returns_list_of_strings(self):
+        "Whether get_os_prepare_command returns a list of command"
+        list_of_preparation_commands = self.working_machine_os.get_os_prepare_commands()
+        self.assertIsInstance(
+            self.working_machine_os.get_os_prepare_commands(), list)
+        for command in list_of_preparation_commands:
+            self.assertIsInstance(command, str)
+
+    def test_get_os_cleanup_commands_returns_list_of_strings(self):
+        "Whether get_os_cleanup_command returns a list of command"
+        list_of_cleanup_commands = self.working_machine_os.get_os_cleanup_commands()
+        self.assertIsInstance(
+            self.working_machine_os.get_os_cleanup_commands(), list)
+        for command in list_of_cleanup_commands:
+            self.assertIsInstance(command, str)
+
+
+class TestArchlinuxInitialization(unittest.TestCase):
+    "Test the initialize method of Archlinux class."
+
+    def setUp(self):
+        """Setup test fixtures"""
+        self.working_machine_os = ArchLinux()
+
+    def test_get_os_install_command_returns_string(self):
+        "Whether get_os_install_command returns a string"
+        self.assertIsInstance(
+            self.working_machine_os.get_os_install_command(), str)
+
+    def test_get_os_update_command_returns_string(self):
+        "Whether get_os_update_command returns a string"
+        self.assertIsInstance(
+            self.working_machine_os.get_os_update_command(), str)
+
+    def test_get_os_prepare_commands_returns_list_of_strings(self):
+        "Whether get_os_prepare_command returns a list of command"
+        list_of_preparation_commands = self.working_machine_os.get_os_prepare_commands()
+        self.assertIsInstance(
+            self.working_machine_os.get_os_prepare_commands(), list)
+        for command in list_of_preparation_commands:
+            self.assertIsInstance(command, str)
+
+    def test_get_os_cleanup_commands_returns_list_of_strings(self):
+        "Whether get_os_cleanup_command returns a list of command"
+        list_of_cleanup_commands = self.working_machine_os.get_os_cleanup_commands()
+        self.assertIsInstance(
+            self.working_machine_os.get_os_cleanup_commands(), list)
+        for command in list_of_cleanup_commands:
+            self.assertIsInstance(command, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
