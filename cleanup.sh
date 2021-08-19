bc258db24f8b7952f6b6542e4d0cd41cf78422bc
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2021-08-19 10:37:17 +0200

    Added yay installation

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,5 @@
 sudo cp -r ./dotfiles/etc/X11/xorg.conf.d/00-keyboard.conf /etc/X11/xorg.conf.d/
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
